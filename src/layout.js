cfed751527703136863f424d79aa8acad85d707c
Nikolay Nemshilov <nemshilov@gmail.com>
2017-11-27 12:14:22 +1100

    Making the checkboxes render good

--- src/layout.js
+++ src/layout.js
@@ -5,19 +5,23 @@ const randomId = () =>
     .toString(16)
     .slice(2, 8)}`;
 
-export default ({ label, error, input, help, id = randomId() }) => (
-  <div className={`form-group${error ? ' has-error' : ''} row`}>
-    <label className="col-sm-3 control-label" htmlFor={id}>
-      {label}
-    </label>
-    <div className="col-sm-9">
-      {React.cloneElement(input, {
-        id,
-        help: undefined,
-        className: 'form-control'
-      })}
+export default ({ label, error, input, help, id = randomId() }) => {
+  const isCheckbox = input.type.toString().includes('Checkbox');
+
+  return (
+    <div className={`form-group${error ? ' has-error' : ''} row`}>
+      <label className="col-sm-3 control-label" htmlFor={id}>
+        {isCheckbox ? null : label}
+      </label>
+      <div className={`col-sm-9${isCheckbox ? ' checkbox' : ''}`}>
+        {React.cloneElement(input, {
+          id,
+          help: undefined,
+          className: isCheckbox ? null : 'form-control'
+        })}
+      </div>
+      {help ? <small className="help-block col-sm-9 col-sm-offset-3">{help}</small> : null}
+      {error ? <small className="text-danger col-sm-9 col-sm-offset-3">{error}</small> : null}
     </div>
-    {help ? <small className="help-block col-sm-9 col-sm-offset-3">{help}</small> : null}
-    {error ? <small className="text-danger col-sm-9 col-sm-offset-3">{error}</small> : null}
-  </div>
-);
+  );
+};
